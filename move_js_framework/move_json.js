93f3f9c73305a3662a9c07e08c52c60c46abebe9
Terry <k9114658@gmail.com>
2014-03-24 23:48:47 +0800

    add move js framework

---
+++ move_js_framework/move_json.js
@@ -0,0 +1,47 @@
+    function getStyle(obj, name){
+        if(obj.currentStyle){
+            return obj.currentStyle[name];
+        }else{
+            return getComputedStyle(obj, false)[name];  //第二個參數放啥都行
+        }
+    }
+    //調用的時候startMove(div, ({ width:100, height:300 }))
+    function startMove(obj, json, fnEnd){   //改成json
+        clearInterval(obj.timer);
+        obj.timer = setInterval(function(){
+            var bStop = true;
+            for(var attr in json){
+                var cur = 0;    //獲取
+                if(attr === 'opacity'){
+                    cur = Math.round( parseFloat( getStyle(obj, attr) )*100 );
+                }else{
+                    cur = parseInt( getStyle(obj, attr) );
+                }
+                
+                var speed = (json[attr] - cur)/6;
+                speed = (speed > 0) ? ( Math.ceil(speed) ) : ( Math.floor(speed) );
+                if(cur != json[attr]){
+                    bStop = false;
+                }
+                if( bStop ){
+                    clearInterval(obj.timer);
+                    
+                    if(fnEnd){  //防止出錯
+                        fnEnd();
+                    }
+                }else{
+                    if(attr === 'opacity'){
+                        obj.style.filter = 'alpha(opacity:' + (cur + speed) + ')';
+                        obj.style.opacity = (cur + speed)/100 ;
+                        //document.getElementById('txt1').value = obj.style.opacity;
+                    }else{
+                        obj.style[attr] = cur + speed + 'px';
+                    }
+                }
+            }
+        }, 30);
+    }
+    /*
+    多物體運動的情況下, 所有的東西都不能共用
+    如果有需要的話, 你完全可以把它們多加一個屬性, 一人一個就不會打架了!
+    */
